00ab944aa765283191e2ffbcee0c878c0a3913e7
RubenWaterman <rubenwaterman@me.com>
2017-04-18 11:27:00 +0300

    update epoch time main net

--- src/main.cpp
+++ src/main.cpp
@@ -2014,7 +2014,7 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1492090137; //epochtime
+        block.nTime    = 1492503980; //epochtime
         block.nBits    = 0x1e0ffff0;
         block.nNonce   = 0;
 

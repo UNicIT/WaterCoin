7a150ea9c1801da44f59cbd7929ab2e5d5ef47c7
RubenWaterman <rubenwaterman@me.com>
2017-04-18 12:36:46 +0300

    generate testnet merkel hash

--- src/main.cpp
+++ src/main.cpp
@@ -2028,7 +2028,7 @@ bool LoadBlockIndex(bool fAllowNew)
         printf("%s\n", block.GetHash().ToString().c_str());
         printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", block.hashMerkleRoot.ToString().c_str());
-        assert(block.hashMerkleRoot == uint256("0x2fc1b7ef46270c053711fbae934cf7f83378efd4b3e158079451d9c6c90e4700"));
+        assert(block.hashMerkleRoot == uint256("0x0ad034eadae7333495c5eb4a99eeb291743cf405857eb865accd39a36d4a92bf"));
 
         // If genesis block hash does not match, then generate new genesis hash.
         if (false && block.GetHash() != hashGenesisBlock)
